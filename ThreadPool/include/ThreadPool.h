31008f9f85fb00546aa0a8d8811980620b507587
zsmj2017 <sphinxs@vip.qq.com>
2018-08-30 11:32:07 +0800

    Add ThreadPool

---
+++ ThreadPool/include/ThreadPool.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <vector>
+#include <queue>
+#include <memory>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <future>
+#include <functional>
+#include <stdexcept>
+
+
+class ThreadPool {
+public:// ctor && dtor
+	explicit ThreadPool(size_t);
+	~ThreadPool();
+
+public: // only interface
+	template<class F,class...Args>
+	auto enqueue(F&& f, Args&&... args)
+		->std::future<std::result_of_t<F(Args...)> >;
+
+private:
+	// keep track of threads so we can join them
+	std::vector<std::thread> workers;
+	// the task queue
+	std::queue<std::function<void()> > tasks;
+
+	// synchronization
+	std::mutex queue_mutex;
+	std::condition_variable condition;
+	bool stop;
+};
+
+template<class F, class ...Args>
+auto ThreadPool::enqueue(F && f, Args && ...args) -> std::future<std::result_of_t<F(Args ...)> >{
+	using return_type = std::result_of_t<F(Args...)>;
+	auto task = std::make_shared<std::packaged_task<return_type()> >(
+		std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+	);
+	std::future<return_type> res = task->get_future();
+	{
+		std::unique_lock<std::mutex> lock(queue_mutex);
+		// don't allow enqueueing after stopping the pool
+		if (stop)
+			throw std::runtime_error("enqueue on stopped ThreadPool");
+		tasks.emplace([task]() { (*task)(); });
+	}
+	condition.notify_one();
+	return res;
+}
